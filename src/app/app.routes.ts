431cc3d52b6c05803f8dbfad11c7c59851827c91
Xongotelo Baloyi <XongoteloB@hollywoodbets.net>
2024-04-18 15:00:00 +0200

    Signup now redirects to the register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './Page/register/register.component';
+import { LoginComponent } from './Page/login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './Page/home/home.component';
+
 
 export const routes: Routes = [
-    ];
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '**', component: PagenotfoundComponent },
+];
+
